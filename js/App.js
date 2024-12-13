bb346ef2292cee632fda972ef7540be9b781c8b6
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-12-13 21:36:08 +0100

    Factory pattern: implements two design pattern in the same time

--- js/App.js
+++ js/App.js
@@ -2,17 +2,29 @@ class App {
     // Constructeur qui initialise les propriétés nécessaires
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper') // Sélectionne l'élément DOM conteneur des cartes de films
+
         this.moviesApi = new MovieApi('/data/new-movie-data.json') // Initialise une instance de MovieApi avec un fichier JSON
+        this.externalMoviesApi = new MovieApi('/data/external-movie-data.json') // Initialise une instance de MovieApi avec un fichier JSON
+
     }
 
     
     // Méthode asynchrone principale qui récupère et affiche les données des films
     async main() {
         const moviesData = await this.moviesApi.getMovies() // Récupère les données des films
+        const externalMoviesData = await this.externalMoviesApi.getMovies() // Récupère les données des films
 
-        moviesData
-            .map(movie => new Movie(movie)) // Convertit chaque objet film en instance de Movie
+        //Ici, transforme mon tableau de données en un tableau de classe Movie
+        const Movies = moviesData.map(movie => new MoviesFactory(movie, 'newApi'))
+        const ExternalMovies = externalMoviesData.map(movie => new MoviesFactory(movie, 'externalApi'))
+
+        console.log(Movies)
+
+        const FullMovies = Movies.concat(ExternalMovies)
+
+        FullMovies
             .forEach(movie => {
+                
                 console.log("====") // Affiche une séparation visuelle dans la console
                 console.log(movie) // Affiche les détails du film dans la console
                 console.log("====") // Affiche une autre séparation visuelle
